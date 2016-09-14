d30d20b85d7954c32ed5e3faf6aa692fbd3468a6
halfmark <tmotimele@yahoo.com>
2016-09-14 10:15:03 +0200

    Add files via upload

--- KMC_Test/KMC.py
+++ KMC_Test/KMC.py
@@ -1,21 +1,35 @@
 import re
+import queue
 
+inputs=['A19','B28','C23','D4','E78','F90','G32','H54','I32','J12','J67','L90','M87','N6','O36','P12','Q24']
 
-Queue=['A19','B28','C23','D4','E78','F90','G32','H54','I32','J12','J67','L90','M87','N6','O36','P12','Q24']
+#sort the inputs **All the older people must enter the premises first**
 
+inputs=sorted(inputs,key=lambda x:int(re.findall(r'\d+$',x)[0]))
+inputs.reverse()
+print(inputs)
+#People must stand in a queue
+q=queue.Queue()
 
 j=0
 z=[]
-for i in Queue:
+for i in inputs:
+    #**No under 18 persons are allowed**
+    #select the age of the person and check if the person is allowed or not
     if int(i[1:]) < 18:
         print ("This person is not allowed ",i)
 
     else:
-        j=j+1
+        #All those who are allowed
+        q.put((int(i[1:]),i))
         z.append(i)
-        print(i)
+       # print(i)
         
     
 
     
 print(z)
+#
+while not q.empty():
+    
+    print(q.get())
